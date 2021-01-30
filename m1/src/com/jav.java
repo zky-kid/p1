63b45cb149dd0ec4ee8b82837e78a629b1077c96
zky-kid <545709811@qq.com>
2021-01-30 15:13:53 +0800

    Initial commit

---
+++ m1/src/com/jav.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.util.Scanner;
+
+public class jav {
+    public static void main(String[] arg) {
+        sysout
+    }
+
+    public void string() {
+        System.out.println("ssad");
+        for (int i = 0; i < ; i++) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
+
+
---
+++ src/com/test.java
@@ -0,0 +1,8 @@
+package com;
+
+public class test {
+    public String st="asd";
+    public static void main(String[] arg){
+        System.out.println("basdbabd");
+    }
+}
